413f3e0f5c14297a5bec3394c0e251eb9ae06988
DOC518 <rhkrwnem@naver.com>
2019-12-13 10:03:32 +0900

    핀 번호를 5으로 1초를 3초로 변경

--- Week5.ino
+++ Week5.ino
@@ -1,4 +1,4 @@
-int ledPin = 7; // ledPin을 7번으로 정의
+﻿int ledPin = 5; // ledPin을 7번으로 정의
 
 void setup() {
   pinMode(ledPin, OUTPUT);    //ledPin을 출력으로 설정
@@ -6,9 +6,9 @@ void setup() {
 
 void loop() {
   digitalWrite(ledPin, HIGH); //LED ON
-  delay(100);                //1초 대기
+  delay(3000);                //1초 대기
   digitalWrite(ledPin, LOW);  //LED OFF
-  delay(100);                //1초 대기
+  delay(3000);                //1초 대기
 }
 
 
